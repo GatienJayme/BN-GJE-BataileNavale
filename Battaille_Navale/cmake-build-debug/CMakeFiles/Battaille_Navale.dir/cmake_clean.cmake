a2febfabd7c5b583d3be09a6b00013976935fa2a
GatienJayme <47849706+GatienJayme@users.noreply.github.com>
2019-03-07 15:58:45 +0100

    Hello Word BN

---
+++ Battaille_Navale/cmake-build-debug/CMakeFiles/Battaille_Navale.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Battaille_Navale.dir/main.c.obj"
+  "Battaille_Navale.pdb"
+  "Battaille_Navale.exe"
+  "Battaille_Navale.exe.manifest"
+  "libBattaille_Navale.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Battaille_Navale.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
